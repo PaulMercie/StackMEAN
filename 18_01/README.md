6ce570ece0b51b0d0d9d4aa18b9e92cc99acfb95
PaulMercie <paulmercie453@gmail.com>
2019-01-18 09:53:23 +0100

    :tada: Template de création de serveur

---
+++ 18_01/README.md
@@ -0,0 +1,13 @@
+# Etape de création d'un serveur NodeJS
+- 1# Créer un fichier "server.js" à la racine du dossier
+- 2# Lancer la commande "npm init -y" (initialiser le serveur)
+- 3# Installer ExpressJS : "npm i -s express"
+- 4# Installer le module "path" pour la gestion du dossier client (dossier www pour les pages HTML...) : "npm i -s path"
+- 5# Installer "ejs" pour configurer le moteur de rendu : "npm i -s ejs"
+- 5# Installer le module pour gérer les variables d'environnement : "npm i -s dotenv"
+- 6# Créer un fichier ".env" pour y intégrer le numéro de port du serveur (Sert à sécuriser les données sensibles)
+- 7# Configuration du serveur (server.js):
+    - Intégrer le module "dotenv"
+    - Intégrer le module "express" dans une constante
+    - Intégrer le module "path" dans une constante
+    - Intégrer le module "ejs" dans une constante
---
+++ 18_01/server.js
@@ -0,0 +1,48 @@
+/*
+Imports
+*/
+    //=> Intégrer "dotenv" (gestion des variables du fichier .env)
+    require('dotenv').config()
+
+    //=> Intégrer le module "express"
+    const express = require('express')
+
+    //=> Intégrer le module "path"
+    const path = require('path')
+
+    //=> Intégrer le module "ejs"
+    const ejs = require('ejs')
+
+/*
+Configuration
+*/
+    // Création du serveur
+    const server = express()
+
+    class ServerClass{
+        // Création de la méthode "init()" --> Initialisation du serveur
+        init(){
+            // Configuartion du dossier client (www)
+            server.set('views', __dirname + '/www')
+            server.use(express.static(path.join(__dirname, 'www')))
+
+            // Configuration du moteur de rendu
+            server.engine('html', ejs.renderFile)
+            server.set('view engine', 'html')
+
+            // Lancer le serveur
+            this.launch()
+        }
+        // Création de la fonction "launch()" --> Ecouter le port de notre serveur
+        launch()
+        {
+            server.listen(process.env.PORT, () => {
+                console.log(`Server listening on port ${process.env.PORT}`)
+            })
+        }
+    }
+
+/*
+Launch
+*/
+    new ServerClass().init()
